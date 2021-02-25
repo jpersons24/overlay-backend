99cfa5e5bdba97583d7987b3067e54ad16bbe400
Jakob Persons <jakobep24@gmail.com>
2021-02-25 13:50:52 -0500

    controller methods

--- app/controllers/favorites_lists_controller.rb
+++ app/controllers/favorites_lists_controller.rb
@@ -1,2 +1,13 @@
 class FavoritesListsController < ApplicationController
+
+   def index
+      favorites_lists = FavoritesList.all
+      render json: favorites_lists
+   end
+
+   def show
+      favorites_list = FavoritesList.find(params[:id])
+      render json: favorites_list
+   end
+
 end
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,11 @@
 class StoriesController < ApplicationController
+   
+   def index
+      stories = Story.all
+      render json: stories
+   end
+
+   def show
+      story = Story.find(params[:id])
+   end
 end
