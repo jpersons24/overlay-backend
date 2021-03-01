9f250f333edd51f59b9d650e90e1c4118a30ae95
Jakob Persons <jakobep24@gmail.com>
2021-03-01 09:22:56 -0500

    quick controller fix

--- app/controllers/favorites_lists_controller.rb
+++ app/controllers/favorites_lists_controller.rb
@@ -9,5 +9,11 @@ class FavoritesListsController < ApplicationController
       fl = FavoritesList.find(params[:id])
       render json: fl
    end
+
+   def create
+      fl_item = FavoritesList.create(user_id: params[:user_id], story_id: params[:story_id])
+      render json: fl_item
+   end
+
    
 end
