831355ea72e8308ebaa67b676cd8806b03602dea
Jakob Persons <jakobep24@gmail.com>
2021-03-01 13:34:49 -0500

    fake auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
+   before_action :authenticate, only: [:show]
+
    def index
       users = User.all
       render json: users
    end
 
    def show
-      user = User.find(params[:id])
-      render json: user
+      render json: @current_user
    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :favorites_lists
   resources :stories
   resources :users
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
