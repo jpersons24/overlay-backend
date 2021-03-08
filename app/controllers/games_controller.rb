a67de1bce825dc5d4f52b96b6bb3290b156e999b
Jakob Persons <jakobep24@gmail.com>
2021-03-08 15:11:45 -0500

    adding create method to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,5 +9,11 @@ class GamesController < ApplicationController
       g = Game.find(params[:id])
       render json: g
    end
+
+   def create
+      newGame = Game.create(sport_key: params[:sport_key], sport_nice: params[:sport_nice], away_team: params[:away_team], home_team: params[:home_team], commence_time: params[:commence_time])
+
+      render json: newGame
+   end
    
 end
