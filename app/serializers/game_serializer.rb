b6bf946ef26fe8c313d48657832cb69e07f3dfbe
Jakob Persons <jakobep24@gmail.com>
2021-03-10 13:38:37 -0500

    changes made

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,6 +1,6 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :sport_key, :sport_nice, :away_team, :home_team, :commence_time
+  attributes :id, :sport_key, :sport_nice, :away_team, :home_team, :commence_time, :sites
 
-  has_many :sites
+  # has_many :sites
   has_many :teams
 end
